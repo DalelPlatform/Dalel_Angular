98f12a8be058d88b078e6c0324d60bb2ca8d264f
ReemAshraf18 <94737100+ReemAshraf18@users.noreply.github.com>
2025-05-07 23:48:51 +0300

    User Auth & Errors Handling

--- src/app/modules/user/user-management-routing.module.ts
+++ src/app/modules/user/user-management-routing.module.ts
@@ -1,22 +1,7 @@
 import { Routes } from "@angular/router";
-<<<<<<< HEAD
-
 export let accountroutes:Routes=[
     {
       // path : 'edit' , component :
     }
 
 ]
-=======
-import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { LogoutComponent } from "./components/logout/logout.component";
-import { CompleteProfilePageComponent } from "./pages/complete-profile-page/complete-profile-page.component";
-export let accountroutes:Routes=[
-    {path:'Login',component:LoginComponent},
-    {path:'Register',component:RegisterComponent},
-    {path:'Logout',component:LogoutComponent},
-    { path: '', redirectTo: 'Login', pathMatch: 'full' },
-    { path: 'complete-profile', component: CompleteProfilePageComponent },
-]
->>>>>>> 0ee238b0146ad6a0348d921fbd696c8f662dd90c
