55fecbe9811405bcbaaab95f60b4389a1200e25e
Osamatalaat <osamatalaat707@gmail.com>
2025-05-07 22:47:01 +0300

    Athorization Guard ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,21 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/user/components/login/login.component';
 import { RegisterComponent } from './modules/user/components/register/register.component';
 import { LogoutComponent } from './modules/user/components/logout/logout.component';
+import { authGuard } from './Services/Guards/auth.guard';
 
 export const routes: Routes = [
-    {
-        path: 'account',
-        loadChildren: () => import('./modules/user/user-module').then(m => m.AccountModule)
-    },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+  {
+    path: 'account',
+    loadChildren: () => import('./modules/user/user-module').then(m => m.AccountModule),
 
-    { path: 'logout', component: LogoutComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full' }
+    canActivate: [authGuard]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  {
+    path: 'logout',
+    component: LogoutComponent,
+    canActivate: [authGuard]
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class UserRoutingModule { }
--- src/app/modules/user/user-management-routing.module.ts
+++ src/app/modules/user/user-management-routing.module.ts
@@ -1,11 +1,8 @@
 import { Routes } from "@angular/router";
-import { LoginComponent } from "./components/login/login.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { LogoutComponent } from "./components/logout/logout.component";
+
 export let accountroutes:Routes=[
-    {path:'Login',component:LoginComponent},
-    {path:'Register',component:RegisterComponent},
-    {path:'Logout',component:LogoutComponent},
-    { path: '', redirectTo: 'Login', pathMatch: 'full' }
-    
+    {
+      // path : 'edit' , component :
+    }
+
 ]
