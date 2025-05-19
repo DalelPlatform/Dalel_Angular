efa71ef64c23120c0107e46da85bea2694c44800
Ahmed Abouzeid <ahmed.abouzeid2018@gmail.com>
2025-05-19 04:05:47 +0300

    solve error

--- src/app/modules/user/user-management-routing.module.ts
+++ src/app/modules/user/user-management-routing.module.ts
@@ -1,31 +1,20 @@
-<<<<<<< HEAD
-// src/app/modules/user/user-management-routing.module.ts
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { RoleGuard } from './services/role.guard'; // Corrected import path
+import { RoleGuard } from './services/role.guard'; // Adjust the import path as necessary
 
 export const accountRoutes: Routes = [
-  {
-    path: 'change-password',
-    component: ChangePasswordComponent,
-    canActivate: [RoleGuard]
-  },
-  { 
-    path: 'forgot-password', 
-    component: ForgotPasswordComponent 
-  },
-  { 
-    path: 'reset-password', 
-    component: ResetPasswordComponent 
-  }
+   
+  { path: 'change-password', component: ChangePasswordComponent, canActivate: [RoleGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(accountRoutes)],
-  exports: [RouterModule]
-});
-
+  imports: [ RouterModule.forChild( accountRoutes) ],
+  exports: [ RouterModule ]
+})
+export class UserManagementRoutingModule { }
