d8c32075b6d40d9d8a40520a17b3b4625448cc72
Mahmoud <mahmoud.m.sayed20@gmail.com>
2025-07-14 20:38:45 +0300

    Add reservations and settings components for restaurant owners, including routing and styling. Implement functionality for managing reservations and updating profile settings. Enhance the add restaurant form with image previews and map integration for location selection.

--- src/app/modules/user/components/login/login.component.ts
+++ src/app/modules/user/components/login/login.component.ts
@@ -1,87 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../../services/user.service';
-import { HttpClient } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie-service';
-import { Router } from '@angular/router';
-import { CompleteProfileServiceProviderService } from '../../../serviceprovider/Services/CompleteProfileServiceProvider.Service'
+  import { Component, OnInit } from '@angular/core';
+  import { AccountService } from '../../services/user.service';
+  import { HttpClient } from '@angular/common/http';
+  import { CookieService } from 'ngx-cookie-service';
+  import { Router } from '@angular/router';
+  import { CompleteProfileServiceProviderService } from '../../../serviceprovider/Services/CompleteProfileServiceProvider.Service'
 
-@Component({
-  selector: 'app-login',
-  standalone: false,
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
-})
-export class LoginComponent {
-  user: { identifier: string; password: string } = {
-    identifier: '',
-    password: ''
-  };
-  constructor(private accountSrv: AccountService,
-    private cookieService: CookieService,
-    private router: Router,
-    private ServiceProviderProfileService: CompleteProfileServiceProviderService,
-  ) { }
+  @Component({
+    selector: 'app-login',
+    standalone: false,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css'
+  })
+  export class LoginComponent {
+    user: { identifier: string; password: string } = {
+      identifier: '',
+      password: ''
+    };
+    showPassword: boolean = false;
+    constructor(private accountSrv: AccountService,
+      private cookieService: CookieService,
+      private router: Router,
+      private ServiceProviderProfileService: CompleteProfileServiceProviderService,
+    ) { }
 
-  
-  Send() {
-  this.accountSrv.Login(this.user.identifier, this.user.password).subscribe({
-    next: (res) => {
-      console.log(res);
-
-        if(res.Image == "empty"){
-          res.Image = "https://t3.ftcdn.net/jpg/03/46/83/96/240_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
-      }
-
-      this.cookieService.set('Token', res.Token);
-      this.cookieService.set('Role', res.Role);
-      this.cookieService.set('Image', res.Image);
-      this.cookieService.set('FullName', res.FullName);
-      this.cookieService.set('Email', res.Email);
-
-        if (res.Role === 'ServiceProvider') {
-          this.ServiceProviderProfileService.checkProfileCompletion().subscribe({
-            next: (isComplete) => {
-              if (!isComplete) {
-                this.router.navigate(['/CompleteProfileServiceProvider']);
-              } else {
-                this.router.navigate(['/ServiceProviderlayout/AllRequests']);
-              }
-            },
-            error: () => {
-              this.router.navigate(['/unauthorized']);
-            }
-          });
-     
-         if (res.Role === 'ServiceProvider') {
-          // this.ServiceProviderProfileService.checkProfileCompletion().subscribe({
-          //   next: (isComplete) => {
-          //     if (!isComplete) {
-          //       this.router.navigate(['/complete-ServiceProvider-profile']);
-          //     } else {
-          //       this.router.navigate(['/account']);
-          //     }
-          //   },
-          //   error: () => {
-          //     this.router.navigate(['/unauthorized']);
-          //   }
-          // });
-        } else {
-          this.router.navigate(['/account']);
-        }
-      
-      }
-    else if(res.Role ==="TravelAgencyOwner"){
-                 this.router.navigate(['/agancy/owner/create-agency']);
-              }
-              else if(res.Role ==="Client"){
-                      this.router.navigate(['/mainPage']);
-              }
-              else{
-                  this.router.navigate(['/login']);
-              }
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
     }
-   
-    });
+    
+    Send() {
+      this.accountSrv.Login(this.user.identifier, this.user.password).subscribe({
+        next: (res) => {
+          console.log(res);
+    
+          if (res.Image == "empty") {
+            res.Image = "https://t3.ftcdn.net/jpg/03/46/83/96/240_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+          }
+    
+          this.cookieService.set('Token', res.Token);
+          this.cookieService.set('Role', res.Role);
+          this.cookieService.set('Image', res.Image);
+          this.cookieService.set('FullName', res.FullName);
+          this.cookieService.set('Email', res.Email);
+    
+          if (res.Role === 'ServiceProvider') {
+            this.ServiceProviderProfileService.checkProfileCompletion().subscribe({
+              next: (isComplete) => {
+                if (!isComplete) {
+                  this.router.navigate(['/CompleteProfileServiceProvider']);
+                } else {
+                  this.router.navigate(['/ServiceProviderlayout/AllRequests']);
+                }
+              },
+              error: () => {
+                this.router.navigate(['/unauthorized']);
+              }
+            });
+          } 
+          else if (res.Role === 'Client') {
+            this.router.navigate(['/mainPage']);
+          } 
+          else if (res.Role === 'PropertyOwner') {
+            this.router.navigate(['/property/owner/']);
+          } 
+          else if (res.Role === 'RestaurantOwner') {
+            this.router.navigate(['/restaurant/owner']);
+          }
+          else if (res.Role === "TravelAgencyOwner") {
+            this.router.navigate(['/agancy/owner/create-agency']);
+          } 
+          else {
+            this.router.navigate(['/login']);
+          }
+        }
+      });
+    }
   }
-
-}
